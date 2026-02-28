a565e7ef5a0782049b73621b87c19e3cb6671636
Junior Dias <juniordiasjfd@gmail.com>
2026-02-28 10:36:22 -0300

    dashboards da página inicial

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from licentia_process.models import Process
 from licentia_process.views import ProcessContextMixin
 from licentia_resources.models import Projeto, Componente
 from django.db.models.functions import Coalesce
-from django.db.models import DecimalField
+from django.db.models import DecimalField, IntegerField
 
 
 class HomeView(LoginRequiredMixin, ProcessContextMixin, TemplateView):
@@ -58,6 +58,36 @@ class HomeView(LoginRequiredMixin, ProcessContextMixin, TemplateView):
             }
             for item in qs.values('status_do_orcamento_aprovacao__nome')
             .annotate(total_valor=Coalesce(Sum('valor_do_processo'), 0, output_field=DecimalField()))
+            .order_by('-total_valor')
+        ]
+
+
+
+        context['contagem_status_do_processo'] = [
+            {
+                'status': item['status_do_processo__nome'] or 'Não definido',
+                'total_valor': int(item['total_valor']) 
+            } 
+            for item in qs.values('status_do_processo__nome')
+            .annotate(total_valor=Coalesce(Sum(1), 0, output_field=IntegerField()))
+            .order_by('-total_valor')
+        ]
+        context['contagem_status_do_orcamento'] = [
+            {
+                'status': item['status_do_orcamento_aprovacao__nome'] or 'Não definido',
+                'total_valor': float(item['total_valor'])
+            }
+            for item in qs.values('status_do_orcamento_aprovacao__nome')
+            .annotate(total_valor=Coalesce(Sum(1), 0, output_field=IntegerField()))
+            .order_by('-total_valor')
+        ]
+        context['contagem_status_do_orcamento_aprovacao'] = [
+            {
+                'status': item['status_do_orcamento_aprovacao__nome'] or 'Não definido',
+                'total_valor': float(item['total_valor'])
+            }
+            for item in qs.values('status_do_orcamento_aprovacao__nome')
+            .annotate(total_valor=Coalesce(Sum(1), 0, output_field=DecimalField()))
             .order_by('-total_valor')
         ]
 
