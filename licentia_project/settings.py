150a900c9225ae0608253cdc71c0284a566129dc
Junior Dias <juniordiasjfd@gmail.com>
2026-03-05 15:27:16 -0300

    desativando e-mail backend

--- licentia_project/settings.py
+++ licentia_project/settings.py
@@ -166,7 +166,12 @@ LOGIN_REDIRECT_URL = 'core:home'
 LOGOUT_REDIRECT_URL = 'users:login'
 
 # Exibe o e-mail de recuperação no terminal do VS Code/Console
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
